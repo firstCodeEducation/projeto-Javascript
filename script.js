db24511237bc0d7519e7cf91873a15df2f102764
IssyPit <isagolp07@gmail.com>
2025-03-04 19:02:40 -0300

    adições no css

---
+++ script.js
@@ -0,0 +1,5 @@
+let standardTheme = document.querySelector('.standard-theme');
+let lightTheme = document.querySelector('.light-theme');
+let darkerTheme = document.querySelector('.darker-theme');
+
+
